e908a5dd921451b4bdcb019998beda29afc770af
Chris Forno <jekor@jekor.com>
2016-06-29 03:31:13 +0800

    Removing async exceptions as they are difficult to reason about and catch properly.

--- lib/Hesh/Process.hs
+++ lib/Hesh/Process.hs
@@ -1,33 +1,45 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE FlexibleInstances #-}
 
 module Hesh.Process ( (|>), (/>), (!>), (&>), (</), (/>>), (!>>), (&>>), pipeOps
-                    , cmd, passThrough, (.=)
+                    , ProcessFailure, cmd, passThrough, (.=)
                     ) where
 
-import Control.Concurrent (forkIO, myThreadId, ThreadId)
-import Control.Exception (finally, bracketOnError, catch, throwTo, SomeException)
+import Control.Exception (Exception, bracketOnError, throwIO)
 import Control.Monad (liftM, void)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.Char (isSpace)
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text.IO
+import Data.Typeable (Typeable)
 import System.Exit (ExitCode(..))
 import System.IO (openFile, IOMode(..), hGetContents, hClose, hPutStrLn, stderr, Handle)
 import System.Process (proc, createProcess, CreateProcess(..), ProcessHandle, waitForProcess, StdStream(..), CmdSpec(..), readProcess, terminateProcess)
 
+type RunningProcess = (String, ProcessHandle)
+type ProcessChain = ([RunningProcess], CreateProcess)
+
+data ProcessFailure = ProcessFailure String Int
+  deriving (Typeable)
+
+instance Show ProcessFailure where
+  show (ProcessFailure command code) = "Command " ++ command ++ " exited with failure code: " ++ show code
+
+instance Exception ProcessFailure
+
 pipeOps = ["|>", "/>", "!>", "&>", "</", "/>>", "!>>", "&>>"]
 
 class PipeResult a where
-  (|>) :: (MonadIO m) => m CreateProcess -> m CreateProcess -> m a
-  (/>) :: (MonadIO m) => m CreateProcess -> FilePath -> m a
-  (!>) :: (MonadIO m) => m CreateProcess -> FilePath -> m a
-  (&>) :: (MonadIO m) => m CreateProcess -> FilePath -> m a
-  (</) :: (MonadIO m) => m CreateProcess -> FilePath -> m a
-  (/>>) :: (MonadIO m) => m CreateProcess -> FilePath -> m a
-  (!>>) :: (MonadIO m) => m CreateProcess -> FilePath -> m a
-  (&>>) :: (MonadIO m) => m CreateProcess -> FilePath -> m a
+  (|>) :: (MonadIO m) => m ProcessChain -> m ProcessChain -> m a
+  (/>) :: (MonadIO m) => m ProcessChain -> FilePath -> m a
+  (!>) :: (MonadIO m) => m ProcessChain -> FilePath -> m a
+  (&>) :: (MonadIO m) => m ProcessChain -> FilePath -> m a
+  (</) :: (MonadIO m) => m ProcessChain -> FilePath -> m a
+  (/>>) :: (MonadIO m) => m ProcessChain -> FilePath -> m a
+  (!>>) :: (MonadIO m) => m ProcessChain -> FilePath -> m a
+  (&>>) :: (MonadIO m) => m ProcessChain -> FilePath -> m a
 
-instance PipeResult CreateProcess where
+instance PipeResult ProcessChain where
   (|>) = pipe
   (</) = redirect [Stdin] ReadMode
   (/>) = redirect [Stdout] WriteMode
@@ -62,8 +74,8 @@ instance PipeResult String where
 class ProcResult a where
   cmd :: (MonadIO m) => FilePath -> [String] -> m a
 
-instance ProcResult CreateProcess where
-  cmd path args = return (proc path args)
+instance ProcResult ProcessChain where
+  cmd path args = return ([], proc path args)
 
 instance ProcResult () where
   cmd path args = passThrough (cmd path args)
@@ -71,20 +83,13 @@ instance ProcResult () where
 instance ProcResult String where
   cmd path args = stdoutToString (cmd path args)
 
-waitForSuccess :: CreateProcess -> ProcessHandle -> IO ()
-waitForSuccess p h = do
-  exit <- waitForProcess h
-  case exit of
-    ExitSuccess -> return ()
-    ExitFailure code -> let (RawCommand command _) = cmdspec p in
-                          error ("Command " ++ command ++ " exited with failure code: " ++ show code)  
-
-asyncWaitForSuccess :: CreateProcess -> ProcessHandle -> IO ()
-asyncWaitForSuccess p h = do
-  parentId <- myThreadId
-  void (forkIO (catch (waitForSuccess p h) (relay parentId)))
- where relay :: ThreadId -> SomeException -> IO ()
-       relay threadId e = throwTo threadId e
+waitForSuccess :: [RunningProcess] -> IO ()
+waitForSuccess hs = mapM_ waitForSuccess' hs
+ where waitForSuccess' (name, handle) = do
+         exit <- waitForProcess handle
+         case exit of
+           ExitSuccess -> return ()
+           ExitFailure code -> throwIO (ProcessFailure name code)
 
 withProcess :: CreateProcess -> ((Maybe Handle, Maybe Handle, Maybe Handle, ProcessHandle) -> IO a) -> IO a
 withProcess p f =
@@ -92,17 +97,20 @@ withProcess p f =
                  (\ (_, _, _, h) -> do terminateProcess h)
                  f
 
-passThrough :: (MonadIO m) => m CreateProcess -> m ()
-passThrough p' = do
-  p <- p'
-  liftIO (withProcess p (\ (_, _, _, pHandle) -> waitForSuccess p pHandle))
+commandName :: CreateProcess -> String
+commandName p = let (RawCommand command _) = cmdspec p in command
 
-stdoutToString :: (MonadIO m) => m CreateProcess -> m String
+passThrough :: (MonadIO m) => m ProcessChain -> m ()
+passThrough p' = do
+  (ps, p) <- p'
+  liftIO (withProcess p (\ (_, _, _, pHandle) -> waitForSuccess (ps ++ [(commandName p, pHandle)])))
+
+stdoutToString :: (MonadIO m) => m ProcessChain -> m String
 stdoutToString p' = do
-  p <- p'
+  (ps, p) <- p'
   liftIO (withProcess (p { std_out = CreatePipe })
                       (\ (_, Just pStdout, _, pHandle) -> do output <- hGetContents pStdout
-                                                             asyncWaitForSuccess p pHandle
+                                                             waitForSuccess (ps ++ [(commandName p, pHandle)])
   -- Strip any trailing newline. These are almost always added to
   -- programs since shells don't add their own newlines, and it's a
   -- surprise to get these when reading a program's output.
@@ -110,24 +118,24 @@ stdoutToString p' = do
                                                                then return (init output)
                                                                else return output))
 
-pipe :: (MonadIO m) => m CreateProcess -> m CreateProcess -> m CreateProcess
+pipe :: (MonadIO m) => m ProcessChain -> m ProcessChain -> m ProcessChain
 pipe p1' p2' = do
-  p1 <- p1'; p2 <- p2'
+  (ps1, p1) <- p1'
+  (ps2, p2) <- p2'
   liftIO (withProcess (p1 { std_out = CreatePipe })
-                      (\ (_, Just p1Stdout, _, p1Handle) -> do asyncWaitForSuccess p1 p1Handle
-                                                               return (p2 { std_in = UseHandle p1Stdout })))
+                      (\ (_, Just p1Stdout, _, p1Handle) -> return (ps1 ++ [(commandName p1, p1Handle)] ++ ps2, p2 { std_in = UseHandle p1Stdout })))
 
 data StdHandle = Stdin | Stdout | Stderr deriving (Eq)
 
-redirect :: (MonadIO m) => [StdHandle] -> IOMode -> m CreateProcess -> FilePath -> m CreateProcess
+redirect :: (MonadIO m) => [StdHandle] -> IOMode -> m ProcessChain -> FilePath -> m ProcessChain
 redirect handles mode p' path = do
-  p <- p'
+  (ps, p) <- p'
   f <- liftIO (openFile path mode)
   return (case handles of
-             [Stdin] -> p { std_in = UseHandle f }
-             [Stdout] -> p { std_out = UseHandle f }
-             [Stderr] -> p { std_err = UseHandle f }
-             [Stdout, Stderr] -> p { std_out = UseHandle f, std_err = UseHandle f })
+             [Stdin] -> (ps, p { std_in = UseHandle f })
+             [Stdout] -> (ps, p { std_out = UseHandle f })
+             [Stderr] -> (ps, p { std_err = UseHandle f })
+             [Stdout, Stderr] -> (ps, p { std_out = UseHandle f, std_err = UseHandle f }))
   -- TODO: Close handle(s).
 
 -- I'm not sure that I want this to stick around, so I'm not
